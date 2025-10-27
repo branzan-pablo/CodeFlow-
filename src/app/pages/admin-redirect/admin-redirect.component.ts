515b5b632cb37e6a5e009e7cb9a88354fbe47575
Pablo Ferreira <branzan.pablo@gmail.com>
2025-10-26 21:54:42 -0300

    feat: Add Angular route for /admin that redirects to Decap CMS

---
+++ src/app/pages/admin-redirect/admin-redirect.component.ts
@@ -0,0 +1,26 @@
+﻿import { Component, inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'app-admin-redirect',
+  standalone: true,
+  template: '<p>Redirecionando para o CMS...</p>',
+  styles: [`
+    :host {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 50vh;
+      font-size: 1.2rem;
+      color: #666;
+    }
+  `],
+})
+export class AdminRedirectComponent {
+  private document = inject(DOCUMENT);
+
+  constructor() {
+    // Redirect to admin using native browser location
+    this.document.location.href = '/admin/index.html';
+  }
+}
